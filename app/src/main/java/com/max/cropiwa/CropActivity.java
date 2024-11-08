bfc4b6e5be2f4daa6c2599e12ae2a83334016493
Dennis Reuben <dennisdestin2@gmail.com>
2024-11-08 14:23:49 +0200

    First Commit

---
+++ app/src/main/java/com/max/cropiwa/CropActivity.java
@@ -0,0 +1,67 @@
+package com.max.cropiwa;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.max.cropiwa.config.CropViewConfigurator;
+import com.yarolegovich.mp.MaterialPreferenceScreen;
+
+import com.max.cropiwa.sample.R;
+
+public class CropActivity extends AppCompatActivity {
+
+    private static final String EXTRA_URI = "https://pp.vk.me/c637119/v637119751/248d1/6dd4IPXWwzI.jpg";
+
+    public static Intent callingIntent(Context context, Uri imageUri) {
+        Intent intent = new Intent(context, CropActivity.class);
+        intent.putExtra(EXTRA_URI, imageUri);
+        return intent;
+    }
+
+    private CropIwaView cropView;
+    private CropViewConfigurator configurator;
+
+    @Override
+    @SuppressWarnings("ConstantConditions")
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.title_crop);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Uri imageUri = getIntent().getParcelableExtra(EXTRA_URI);
+        cropView = (CropIwaView) findViewById(R.id.crop_view);
+        cropView.setImageUri(imageUri);
+
+        MaterialPreferenceScreen cropPrefScreen = (MaterialPreferenceScreen) findViewById(R.id.crop_preference_screen);
+        configurator = new CropViewConfigurator(cropView, cropPrefScreen);
+        cropPrefScreen.setStorageModule(configurator);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_crop, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.done) {
+            cropView.crop(configurator.getSelectedSaveConfig());
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven(uri("https://jitpack.io"))
+
+        maven { url = uri("https://maven.google.com") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(uri("https://jitpack.io"))
+
+    }
+}
+
+rootProject.name = "Cropiwa"
+include(":app")
+include(":library")
